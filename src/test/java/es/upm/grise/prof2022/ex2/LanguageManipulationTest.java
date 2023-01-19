0b46df07fadcd2875110217f6a569372b109488e
paulalaban <p.labandeira@alumnos.upm.es>
2023-01-19 17:15:36 +0100

    tercera parte examen

--- src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
+++ src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
@@ -5,24 +5,27 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 
 public class LanguageManipulationTest {
 
 	@Test
 	public void testPath1() throws Exception { // Camino 15 -18 - 19 - 23 - 24 - 26 - NF
-		LanguageManipulation lm = new LanguageManipulation();
+		LanguageManipulation lm = Mockito.mock(LanguageManipulation.class);
 		Assertions.assertThrows(CannotFindPropertyFileOrWrongFileException.class, () ->  lm.getText("greeting", Language.Italian));
+
 	}
 	
 	@Test
 	public void testPath2() throws Exception { // Camino 15 -18 - 19 - 23 - 24 - 30 - 33a - 33b - 34 - NF
-		LanguageManipulation lm = new LanguageManipulation();
+		LanguageManipulation lm = Mockito.mock(LanguageManipulation.class);
 		Assertions.assertThrows(NonExistingKeyException.class, () ->  lm.getText("bye", Language.English));
 	}
 	
 	@Test
 	public void testPath3() throws Exception { // Camino 15 -18 - 19 - 23 - 24 - 30 - 33a - 38a - 38b - 39 - 42 - NF
-		LanguageManipulation lm = new LanguageManipulation();
+		LanguageManipulation lm = Mockito.mock(LanguageManipulation.class);
 		assertEquals("This Key does not exist or it has not been yet translated",lm.getText("adios", Language.Spanish));
 	}
 
