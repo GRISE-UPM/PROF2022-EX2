c56268a20cad3ceec3b534e53c95cceab11491d8
Sandra Sierra <sandra.sierra@vipera.com>
2023-01-19 17:16:46 +0100

    Tercera parte del examen

--- src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
+++ src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
@@ -2,10 +2,26 @@ package es.upm.grise.prof2022.ex2;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class LanguageManipulationTest {
+
+	static Collaborator collaborator;
+	static LanguageManipulation lm;
+	static String key = "farewell";
+	static String text = "This Key does not exist or it has not been yet translated";
+
+	@BeforeAll
+	static void setUp() {
+		collaborator = mock(Collaborator.class);
+
+		lm = new LanguageManipulation();
+		lm.setCollaborator(collaborator);
+	}
 
 	/*
 	 * C2: 15,18,19 - 24 - 26 - 43
@@ -22,9 +38,8 @@ public class LanguageManipulationTest {
 	 */
 	@Test
 	public void firstIfTest() {
-		LanguageManipulation lm = new LanguageManipulation();
-		assertThrows(NonExistingKeyException.class,
-				() -> lm.getText("farewell", Language.English));
+		when(collaborator.getProperty(key)).thenReturn(null);
+		assertThrows(NonExistingKeyException.class, () -> lm.getText(key, Language.English));
 	}
 
 	/*
@@ -32,8 +47,7 @@ public class LanguageManipulationTest {
 	 */
 	@Test
 	public void secondIfTest() throws Exception {
-		LanguageManipulation lm = new LanguageManipulation();
-		assertEquals("This Key does not exist or it has not been yet translated",
-				lm.getText("farewell", Language.Spanish));
+		when(collaborator.getProperty(key)).thenReturn(text);
+		assertEquals(text, lm.getText(key, Language.Spanish));
 	}
 }
