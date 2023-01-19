4ced42a7ef6f67924619048978f2708ad966552e
Pabloabril <p.deandres@alumnos.upm.es>
2023-01-19 16:14:45 +0100

    Primera parte del examen

--- src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
+++ src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
@@ -6,13 +6,29 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 public class LanguageManipulationTest {
+	
+	@Test
+	public void property_file_error_test() throws Exception{
+		LanguageManipulation lm = new LanguageManipulation();;
+		Language lan = Language.Italian;
+		assertThrows(CannotFindPropertyFileOrWrongFileException.class,()->lm.getText(null,lan));
+	}
 
 	@Test
-	public void smokeTest() throws Exception {
+	public void null_text_and_english_language_test() throws Exception{
 		LanguageManipulation lm = new LanguageManipulation();
-		assertEquals("How are you?", lm.getText("greeting", Language.English));
-		assertEquals("¿Cómo se encuentra?", lm.getText("greeting", Language.Spanish));
-		assertThrows(CannotFindPropertyFileOrWrongFileException.class, () -> lm.getText("greeting", Language.Italian));
+		String key = "no_property";
+		Language lan = Language.English;
+		assertThrows(NonExistingKeyException.class,()->lm.getText(key,lan))
+	}
+
+	@Test
+	public void text_and_spanish_languge_test() throws Exception{
+		LanguageManipulation lm = new LanguageManipulation();
+		String key = "no_property";
+		Language lan = Language.Spanish;
+		String text = "This Key does not exist or it has not been yet translated";
+		assertEquals(text,lm.getText(key,lan));
 	}
 
 }
