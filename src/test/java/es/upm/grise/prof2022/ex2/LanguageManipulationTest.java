e14bd60de1f511ba891449c92aa5e1bc3d551e6a
Juan Viejo <juanalfonso.viejo.rodriguez@alumnos.upm.es>
2023-01-19 15:45:18 +0100

    Primera parte del examen

--- src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
+++ src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
@@ -1,18 +1,61 @@
 package es.upm.grise.prof2022.ex2;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+//Los numeros de los caminos hacen referencia a la imagen del ejercicio 4
+//y la imagen de este repositorio resources/imagen.jpeg
 public class LanguageManipulationTest {
 
+	/*
+	 * Camino 1
+	 * Ejercitamos el catch
+	 */
 	@Test
-	public void smokeTest() throws Exception {
+	public void camino1Test() {
 		LanguageManipulation lm = new LanguageManipulation();
-		assertEquals("How are you?", lm.getText("greeting", Language.English));
-		assertEquals("¿Cómo se encuentra?", lm.getText("greeting", Language.Spanish));
-		assertThrows(CannotFindPropertyFileOrWrongFileException.class, () -> lm.getText("greeting", Language.Italian));
+		assertThrows(CannotFindPropertyFileOrWrongFileException.class, () -> {
+			lm.getText("key", Language.Italian);
+		});
+	}
+
+	/*
+	 * Camino 2
+	 * Ejercitamos el primer if, lanzamos excepcion
+	 */
+	@Test
+	public void camino2Test() {
+		LanguageManipulation lm = new LanguageManipulation();
+		try {
+			lm.getText("greeting", Language.English);
+		} catch (Exception e) {
+			fail();
+			e.printStackTrace();
+		}
+
+		assertThrows(NonExistingKeyException.class, () -> {
+			lm.getText("key", Language.English);
+		});
+	}
+
+	/*
+	 * Camino 6
+	 * Ejercitamos el segundo if, obtenemos un mensaje
+	 */
+	@Test
+	public void camino6Test() {
+		LanguageManipulation lm = new LanguageManipulation();
+		String text = "";
+		String res = "This Key does not exist or it has not been yet translated";
+		try {
+			text = lm.getText("key", Language.Spanish);
+		} catch (Exception e) {
+			fail();
+			e.printStackTrace();
+		}
+		assertEquals(res, text);
+		
 	}
 
 }
