2056705ad0b0d234c179eb08874284b3579dd310
Adrian Tabacaru <adrian.t-@alumnos.upm.es>
2023-01-19 16:10:21 +0100

    Segunda parte del examen

---
+++ src/main/java/es/upm/grise/prof2022/ex2/FileReader.java
@@ -0,0 +1,21 @@
+package es.upm.grise.prof2022.ex2;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class FileReader{
+    public Properties getPropertiesFile(Language language) throws CannotFindPropertyFileOrWrongFileException {
+        // Property filename
+        String fileName = language.toString() + "-strings.properties";
+        Properties propertyFile = new Properties();
+
+        // Load the property fileException
+        // If the file does not exist, we raise an exception
+        try {
+            propertyFile.load(new FileInputStream("resources/" + fileName));
+        } catch (Exception e) {
+            throw new CannotFindPropertyFileOrWrongFileException();
+        }
+        return propertyFile;
+    }
+}
--- src/main/java/es/upm/grise/prof2022/ex2/LanguageManipulation.java
+++ src/main/java/es/upm/grise/prof2022/ex2/LanguageManipulation.java
@@ -5,6 +5,9 @@ import java.util.Properties;
 
 public class LanguageManipulation {
 
+
+	FileReader reader = new FileReader();
+
 	// The strings are stored in property files under /resources
 	// Each file corresponds to a Language
 	// This method return the string associated with a key, e.g.,
@@ -14,17 +17,7 @@ public class LanguageManipulation {
 	// See the smokeTest
 	public String getText(String key, Language language) throws Exception {
 
-		// Property filename
-		String fileName = language.toString() + "-strings.properties";
-		Properties propertyFile = new Properties();
-
-		// Load the property fileException
-		// If the file does not exist, we raise an exception
-		try {
-			propertyFile.load(new FileInputStream("resources/" + fileName));
-		} catch (Exception e) {
-			throw new CannotFindPropertyFileOrWrongFileException();
-		}
+		Properties propertyFile = reader.getPropertiesFile(language);
 
 		// Find the requested string
 		String text = propertyFile.getProperty(key);
