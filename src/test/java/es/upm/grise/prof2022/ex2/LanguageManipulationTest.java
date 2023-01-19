d5f7b6016bbbd8b6613e6ae50bc49730b326bac9
leticia <leticiacohenferrari@gmail.com>
2023-01-19 16:55:25 +0100

    Tercera parte del examen

--- src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
+++ src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
@@ -2,6 +2,8 @@ package es.upm.grise.prof2022.ex2;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,23 +11,27 @@ public class LanguageManipulationTest {
 	
 	// camino basico 1: 15-23-25
 	@Test
-	public void unavailableLanguageFileTest(){
+	public void unavailableLanguageFileTest() throws CannotFindPropertyFileOrWrongFileException{
 		LanguageManipulation lm = new LanguageManipulation();
-		assertThrows(CannotFindPropertyFileOrWrongFileException.class,() -> lm.getText("greeting", Language.Italian, new LanguageFile()));
+		LanguageFile languageFile = mock(LanguageFile.class);
+		when(languageFile.getText("greeting", Language.Italian)).thenThrow(CannotFindPropertyFileOrWrongFileException.class);
+		assertThrows(CannotFindPropertyFileOrWrongFileException.class,() -> lm.getText("greeting", Language.Italian, languageFile));
 	}
 	
 	// camino basico 2: 15-23-33a-33b-34
 	@Test
 	public void emptyWordInEnglishTest() throws Exception {
 		LanguageManipulation lm = new LanguageManipulation();
-		assertThrows(NonExistingKeyException.class,() -> lm.getText("", Language.English, new LanguageFile()));
+		LanguageFile languageFile = mock(LanguageFile.class);
+		assertThrows(NonExistingKeyException.class,() -> lm.getText("", Language.English, languageFile));
 	}
 	
 	// camino basico 3: 15-23-33a-33b-38a-38b-39-42
 	@Test
 	public void emptyWordNotInEnglishTest() throws Exception {
 		LanguageManipulation lm = new LanguageManipulation();
-		assertEquals("This Key does not exist or it has not been yet translated", lm.getText("", Language.Spanish, new LanguageFile()));
+		LanguageFile languageFile = mock(LanguageFile.class);
+		assertEquals("This Key does not exist or it has not been yet translated", lm.getText("", Language.Spanish, languageFile));
 	}
 
 }
