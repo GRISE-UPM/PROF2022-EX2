ce488dc1d7349e52a2cd3264bf0c7fdaeb990190
Esteban Carrillo <me.carrillo@alumnos.upm.es>
2023-01-19 16:22:02 +0100

    Primera parte del examen

--- src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
+++ src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
@@ -7,12 +7,41 @@ import org.junit.jupiter.api.Test;
 
 public class LanguageManipulationTest {
 
+	
+	/**
+	 * Prueba de camino básico 1: // Load the property fileException
+	 */
 	@Test
-	public void smokeTest() throws Exception {
+	public void catchException_test() {
 		LanguageManipulation lm = new LanguageManipulation();
-		assertEquals("How are you?", lm.getText("greeting", Language.English));
-		assertEquals("¿Cómo se encuentra?", lm.getText("greeting", Language.Spanish));
+		
 		assertThrows(CannotFindPropertyFileOrWrongFileException.class, () -> lm.getText("greeting", Language.Italian));
 	}
+	
+	
+	/**
+	 * Prueba de camino básico 2: Non-existing key: case 1
+	 */
+	@Test
+	public void NonExistingKeyException_test() {
+		LanguageManipulation lm = new LanguageManipulation();
+		
+		
+		assertThrows(NonExistingKeyException.class, () -> lm.getText("", Language.English));
+	}
+	
 
+	/**
+	 * Prueba de camino básico 5:  Non-existing key: case 2
+	 * @throws Exception 
+	 */
+	@Test
+	public void NonExistingKey_test() throws Exception {
+		LanguageManipulation lm = new LanguageManipulation();
+		String result = "This Key does not exist or it has not been yet translated";
+		
+		
+		assertEquals(result, lm.getText("saludo", Language.Spanish));
+	}
+	
 }
