f3267edc6d7fa577940315c4e463163a310f4e89
Álvaro López García <alvarolopez769@gmail.com>
2023-01-19 16:24:56 +0100

    Primera parte del examen

--- src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
+++ src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
@@ -2,17 +2,75 @@ package es.upm.grise.prof2022.ex2;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class LanguageManipulationTest {
-
+	
+	// Variables
+	private static LanguageManipulation lm;
+	private String ky;
+	private Language lg;
+	
+	@BeforeAll
+	public static void init () {
+		
+		// Initialization
+		lm = new LanguageManipulation();
+	}
+	
+	/*
+	 * Test relativo al camino C1:
+	 * 	Llamada - 15 - 26 - Salida
+	 */
 	@Test
-	public void smokeTest() throws Exception {
-		LanguageManipulation lm = new LanguageManipulation();
-		assertEquals("How are you?", lm.getText("greeting", Language.English));
-		assertEquals("¿Cómo se encuentra?", lm.getText("greeting", Language.Spanish));
-		assertThrows(CannotFindPropertyFileOrWrongFileException.class, () -> lm.getText("greeting", Language.Italian));
+	public void catchTest() {
+		
+		// Initialization
+		ky = "foo";
+		lg = Language.Italian;
+		
+		// Assertion
+		assertThrows(CannotFindPropertyFileOrWrongFileException.class, () -> {
+			lm.getText(ky, lg);
+		});
+	}
+	
+	/*
+	 * Test relativo al camino C2:
+	 * 	Llamada - 15 - 28 - 33 - 34 - Salida
+	 */
+	@Test
+	public void if1Test() {
+		
+		// Initialization
+		ky = "foo";
+		lg = Language.English;
+		
+		// Assertion
+		assertThrows(NonExistingKeyException.class, () -> {
+			lm.getText(ky, lg);
+		});
 	}
 
+	/*
+	 * Test relativo al camino C6:
+	 * 	Llamada - 15 - 28 - 38a - 38b - 39 - 42 - Salida
+	 */
+	@Test
+	public void if2Test() {
+	
+		// Initialization
+		ky = "foo";
+		lg = Language.Spanish;
+
+		// Assertion
+		try {
+			assertEquals(lm.getText(ky, lg), "This Key does not exist or it has not been yet translated");
+		} catch (Exception e) {
+			fail("No debería haber lanzado Exception");
+		}
+	}
 }
