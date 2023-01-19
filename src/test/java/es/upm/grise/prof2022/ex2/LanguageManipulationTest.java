06d71642ace1205c2a7bb138cd40e5470023dab0
Alberto Pérez <alberto.perezb@alumnos.upm.es>
2023-01-19 16:12:14 +0100

    Primera parte del examen

--- src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
+++ src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
@@ -7,12 +7,31 @@ import org.junit.jupiter.api.Test;
 
 public class LanguageManipulationTest {
 
+	
 	@Test
-	public void smokeTest() throws Exception {
+	//Primer catch
+	//Corresponde a mi path: 1-2-FIN
+	public void primerCatch() throws Exception {
 		LanguageManipulation lm = new LanguageManipulation();
-		assertEquals("How are you?", lm.getText("greeting", Language.English));
-		assertEquals("¿Cómo se encuentra?", lm.getText("greeting", Language.Spanish));
 		assertThrows(CannotFindPropertyFileOrWrongFileException.class, () -> lm.getText("greeting", Language.Italian));
-	}
+	} 
+	
+	@Test
+	//Primer if
+	//Corresponde a mi path: 1-3a-3b-5-FIN
+	public void TextNull_AND_English() throws Exception {
+		LanguageManipulation lm = new LanguageManipulation();
+		assertThrows(NonExistingKeyException.class, () -> lm.getText("", Language.English));
+	} 
+	
+	@Test
+	//Segundo if
+	//Corresponde a mi path: 1-3a-3b-4a-4b-7-6-FIN
+	public void TextNull_AND_Not_English() throws Exception {
+		LanguageManipulation lm = new LanguageManipulation();
+		assertEquals("This Key does not exist or it has not been yet translated", lm.getText("",Language.Spanish ));
+		
+	} 
+
 
 }
