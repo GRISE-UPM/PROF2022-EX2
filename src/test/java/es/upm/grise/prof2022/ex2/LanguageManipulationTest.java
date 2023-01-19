434b9d6f4807c51fea26ec99072b718cf2ee9947
Mario de Lucas <delucasgarcia.mario@gmail.com>
2023-01-19 17:16:21 +0100

    Tercer parte del examen

--- src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
+++ src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
@@ -3,14 +3,20 @@ package es.upm.grise.prof2022.ex2;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Properties;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 public class LanguageManipulationTest {
+
+	public LanguageManipulation lm = new LanguageManipulation();
 
 	// Test - First catch block
 	@Test
 	public void testFirstCatchBlock() throws Exception {
-		LanguageManipulation lm = new LanguageManipulation();
+		lm.propertiesColaborator = Mockito.mock(PropertiesColaborator.class);
+		Mockito.when(lm.propertiesColaborator.loadColaborator("DontExistProperties")).thenThrow(new CannotFindPropertyFileOrWrongFileException());
 		assertThrows(CannotFindPropertyFileOrWrongFileException.class, () -> lm.getText("does-not-matter", Language.Italian));
 	}
 
@@ -18,7 +24,8 @@ public class LanguageManipulationTest {
 	// key parameter is set to non-existing-key since there is only one key in the English property file called greeting
 	@Test
 	public void testNonExistingKeyInEnglish() throws Exception {
-		LanguageManipulation lm = new LanguageManipulation();
+		lm.propertiesColaborator = Mockito.mock(PropertiesColaborator.class);
+		Mockito.when(lm.propertiesColaborator.loadColaborator("English-strings.properties")).thenReturn(new Properties());
 		assertThrows(NonExistingKeyException.class, () -> lm.getText("non-existing-key", Language.English));
 	}
 
@@ -26,7 +33,8 @@ public class LanguageManipulationTest {
 	// key parameter is set to clave-inexistente since there is only one key in the Spanish property file called greeting
 	@Test
 	public void testNonExistingKeyInSpanish() throws Exception {
-		LanguageManipulation lm = new LanguageManipulation();
+		lm.propertiesColaborator = Mockito.mock(PropertiesColaborator.class);
+		Mockito.when(lm.propertiesColaborator.loadColaborator("Spanish-strings.properties")).thenReturn(new Properties());
 		assertEquals("This Key does not exist or it has not been yet translated", lm.getText("clave-inexistente", Language.Spanish));
 	}
 }
