922622c6c9bdeab0a11aa40c5f381618258307a9
Mario de Lucas <delucasgarcia.mario@gmail.com>
2023-01-19 16:07:58 +0100

    Primera parte del examen

--- src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
+++ src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
@@ -7,12 +7,26 @@ import org.junit.jupiter.api.Test;
 
 public class LanguageManipulationTest {
 
+	// Test - First catch block
 	@Test
-	public void smokeTest() throws Exception {
+	public void testFirstCatchBlock() throws Exception {
 		LanguageManipulation lm = new LanguageManipulation();
-		assertEquals("How are you?", lm.getText("greeting", Language.English));
-		assertEquals("¿Cómo se encuentra?", lm.getText("greeting", Language.Spanish));
-		assertThrows(CannotFindPropertyFileOrWrongFileException.class, () -> lm.getText("greeting", Language.Italian));
+		assertThrows(CannotFindPropertyFileOrWrongFileException.class, () -> lm.getText("does-not-matter", Language.Italian));
 	}
 
+	// Test - non-existing key in English
+	// key parameter is set to non-existing-key since there is only one key in the English property file called greeting
+	@Test
+	public void testNonExistingKeyInEnglish() throws Exception {
+		LanguageManipulation lm = new LanguageManipulation();
+		assertThrows(NonExistingKeyException.class, () -> lm.getText("non-existing-key", Language.English));
+	}
+
+	// Test - non-existing key in Spanish
+	// key parameter is set to clave-inexistente since there is only one key in the Spanish property file called greeting
+	@Test
+	public void testNonExistingKeyInSpanish() throws Exception {
+		LanguageManipulation lm = new LanguageManipulation();
+		assertEquals("This Key does not exist or it has not been yet translated", lm.getText("clave-inexistente", Language.Spanish));
+	}
 }
