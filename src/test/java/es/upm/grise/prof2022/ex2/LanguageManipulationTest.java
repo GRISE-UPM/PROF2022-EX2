5535c018a638ddf2e7c788842024a50e596c9362
Sandra Sierra <sandra.sierra@vipera.com>
2023-01-19 16:13:20 +0100

    Primera parte del examen

--- src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
+++ src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
@@ -7,12 +7,33 @@ import org.junit.jupiter.api.Test;
 
 public class LanguageManipulationTest {
 
+	/*
+	 * C2: 15,18,19 - 24 - 26 - 43
+	 */
 	@Test
-	public void smokeTest() throws Exception {
+	public void catchTest() {
 		LanguageManipulation lm = new LanguageManipulation();
-		assertEquals("How are you?", lm.getText("greeting", Language.English));
-		assertEquals("¿Cómo se encuentra?", lm.getText("greeting", Language.Spanish));
-		assertThrows(CannotFindPropertyFileOrWrongFileException.class, () -> lm.getText("greeting", Language.Italian));
+		assertThrows(CannotFindPropertyFileOrWrongFileException.class,
+				() -> lm.getText("greeting", Language.Italian));
 	}
 
+	/*
+	 * C6: 15,18,19 - 24 - 30 - 33a - 33b - 34 - 43
+	 */
+	@Test
+	public void firstIfTest() {
+		LanguageManipulation lm = new LanguageManipulation();
+		assertThrows(NonExistingKeyException.class,
+				() -> lm.getText("farewell", Language.English));
+	}
+
+	/*
+	 * C8: 15,18,19 - 24 - 30 - 33a - 38a - 38b - 39 - 42 - 43
+	 */
+	@Test
+	public void secondIfTest() throws Exception {
+		LanguageManipulation lm = new LanguageManipulation();
+		assertEquals("This Key does not exist or it has not been yet translated",
+				lm.getText("farewell", Language.Spanish));
+	}
 }
