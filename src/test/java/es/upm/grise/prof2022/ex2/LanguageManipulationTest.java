b4932b462a411c09ffdf72b839d03406679ebe9a
Adrian Tabacaru <adrian.t-@alumnos.upm.es>
2023-01-19 15:56:54 +0100

    Added Tests

--- src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
+++ src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
@@ -3,16 +3,28 @@ package es.upm.grise.prof2022.ex2;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedConstruction;
+import org.mockito.Mockito;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.util.Properties;
 
 public class LanguageManipulationTest {
 
+	LanguageManipulation testedClass = new LanguageManipulation();
 	@Test
-	public void smokeTest() throws Exception {
-		LanguageManipulation lm = new LanguageManipulation();
-		assertEquals("How are you?", lm.getText("greeting", Language.English));
-		assertEquals("¿Cómo se encuentra?", lm.getText("greeting", Language.Spanish));
-		assertThrows(CannotFindPropertyFileOrWrongFileException.class, () -> lm.getText("greeting", Language.Italian));
+	public void nonExistingKeyExceptionTest() { // Corresponde al primer if
+		Assertions.assertThrows(NonExistingKeyException.class , () -> testedClass.getText("noKey",Language.English));
+	}
+
+	@Test
+	public void nonExistingKeySpanishTest() throws Exception { // Corresponde al segundo if
+		Assertions.assertEquals("This Key does not exist or it has not been yet translated" , testedClass.getText("noKey",Language.Spanish));
 	}
 
 }
