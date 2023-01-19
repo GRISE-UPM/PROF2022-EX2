16d2df549e54ea9f5c330707bc9b9cae6ae68d37
unknown <sara.romano@fabrick.com>
2023-01-19 16:04:12 +0100

    Primera parte del examen

--- src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
+++ src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
@@ -7,12 +7,33 @@ import org.junit.jupiter.api.Test;
 
 public class LanguageManipulationTest {
 
+//	@Test
+//	public void smokeTest() throws Exception {
+//		LanguageManipulation lm = new LanguageManipulation();
+//		assertEquals("How are you?", lm.getText("greeting", Language.English));
+//		assertEquals("¿Cómo se encuentra?", lm.getText("greeting", Language.Spanish));
+//		assertThrows(CannotFindPropertyFileOrWrongFileException.class, () -> lm.getText("greeting", Language.Italian));
+//	}
+
+	LanguageManipulation lm = new LanguageManipulation();
+
+	//test correspondiente al Path I
 	@Test
-	public void smokeTest() throws Exception {
-		LanguageManipulation lm = new LanguageManipulation();
-		assertEquals("How are you?", lm.getText("greeting", Language.English));
-		assertEquals("¿Cómo se encuentra?", lm.getText("greeting", Language.Spanish));
+	public void testGetTextShouldThrowFindPropertyException() throws Exception {
 		assertThrows(CannotFindPropertyFileOrWrongFileException.class, () -> lm.getText("greeting", Language.Italian));
 	}
+
+	//test correspondiente al Path II
+	@Test
+	public void testGetTextShouldThrowNonExistingException() throws Exception {
+		assertThrows(NonExistingKeyException.class, () -> lm.getText("welcome", Language.English));
+	}
+
+	//test correspondiente al Path III
+	@Test
+	public void testGetTextShouldReturnTextNonExisting() throws Exception {
+		assertEquals("This Key does not exist or it has not been yet translated", lm.getText("welcome", Language.Spanish));
+	}
+
 
 }
