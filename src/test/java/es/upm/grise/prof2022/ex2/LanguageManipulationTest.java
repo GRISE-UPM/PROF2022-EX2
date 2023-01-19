e790db0a59053945bd3482e4d60dc9859788cd81
juan <juan@MSI.home>
2023-01-19 16:24:05 +0100

    Tercera parte del examen

--- src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
+++ src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
@@ -13,25 +13,28 @@ public class LanguageManipulationTest {
 	@Test
 	public void path1() throws Exception {
 		LanguageManipulation lm = new LanguageManipulation();
-		//AccesoFicheros real = mock();
-		//AccesoFicheros af = mock(AccesoFicheros.class);
-		SuperDiccionario af = new SuperDiccionario();
-
-		//when(af.getDiccionario(any(Language.class))).thenReturn("Good morning");
-
+		
+		SuperDiccionario af = mock(SuperDiccionario.class);
+		when(af.traducir(any(Language.class),any(String.class))).thenReturn(null);
 		
 		assertThrows(CannotFindPropertyFileOrWrongFileException.class, () -> lm.getText("greeting", Language.Italian,af));
 	}
 	@Test
 	public void path2() throws Exception {
 		LanguageManipulation lm = new LanguageManipulation();
-		SuperDiccionario af = new SuperDiccionario();
+		
+		SuperDiccionario af = mock(SuperDiccionario.class);
+		when(af.traducir(any(Language.class),any(String.class))).thenReturn("How are you?");
+		
 		assertEquals("How are you?", lm.getText("greeting", Language.English,af));
 	}
 	@Test
 	public void path3() throws Exception {
 		LanguageManipulation lm = new LanguageManipulation();
-		SuperDiccionario af = new SuperDiccionario();
+		
+		SuperDiccionario af = mock(SuperDiccionario.class);
+		when(af.traducir(any(Language.class),any(String.class))).thenReturn(null);
+		
 		assertEquals("This Key does not exist or it has not been yet translated", lm.getText("tude mal?", Language.Spanish,af));
 	}
 }
