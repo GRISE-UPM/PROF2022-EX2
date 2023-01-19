3f65caed2ca6ab65ce1a7a5888f7173527a78161
Jorge Marrero Camiruaga <57073594+Jorge-Marrero@users.noreply.github.com>
2023-01-19 16:04:54 +0100

    Primera parte del examen

--- src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
+++ src/test/java/es/upm/grise/prof2022/ex2/LanguageManipulationTest.java
@@ -8,11 +8,21 @@ import org.junit.jupiter.api.Test;
 public class LanguageManipulationTest {
 
 	@Test
-	public void smokeTest() throws Exception {
+	public void nonExistingKeyEnglishTest() throws Exception {
 		LanguageManipulation lm = new LanguageManipulation();
-		assertEquals("How are you?", lm.getText("greeting", Language.English));
-		assertEquals("¿Cómo se encuentra?", lm.getText("greeting", Language.Spanish));
-		assertThrows(CannotFindPropertyFileOrWrongFileException.class, () -> lm.getText("greeting", Language.Italian));
+		assertThrows(NonExistingKeyException.class, () -> lm.getText("nada", Language.English));
+	}
+	
+	@Test
+	public void nonExistingKeyNoEnglishTest() throws Exception {
+		LanguageManipulation lm = new LanguageManipulation();
+		assertEquals("This Key does not exist or it has not been yet translated", lm.getText("nada", Language.Spanish));
+	}
+	
+	@Test
+	public void fileNameNonExist() throws Exception {
+		LanguageManipulation lm = new LanguageManipulation();
+		assertThrows(CannotFindPropertyFileOrWrongFileException.class, () -> lm.getText("French", Language.Italian));
 	}
 
 }
